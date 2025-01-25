61d4f76ec76917a8c8693c8651fbd1c9cb693322
Ratan-Y-mallya <ymratanmallya@gmail.com>
2025-01-25 22:44:37 +0530

    divider

--- 32-bit division/Divider.v
+++ 32-bit division/Divider.v
@@ -5,27 +5,27 @@ input [31:0] in1,in2;
 output [31:0]final;
 
 // interim wire
-wire [22:0]M1,M2;
+wire [23:0]M1,M2;
 wire [7:0]E1,E2;
 wire s1,s2;
 wire[8:0] eigth_bit_sub,nine_bit_add;
-wire [47:0] multi;
+wire [31:0] div;
 
 // Design of the circuit
 
-assign M1 =  in1[22:0];
-assign M2 =  in2[22:0];
+assign M1 = {1'b1, in1[22:0]};
+assign M2 = {1'b1, in2[22:0]};
 assign E1 =  in1[30:23];
 assign E2 =  in2[30:23];    
 
-assign eigth_bit_sub = E1 + E2;
-assign eigth_bit_add = eigth_bit_sub - 9'b001111111;
+assign eigth_bit_sub = E1 - E2;
+assign eigth_bit_add = eigth_bit_sub + 9'b001111111;
 
 assign  final[30:23] = eigth_bit_add;
 
-assign multi = M1*M2;
+assign div = M1/M2;
 
-assign final[22:0] = multi;
+assign final[22:0] = div;
 
 assign s1 = in1[31];
 assign s2 = in2[31];
--- 32-bit multiplier/Multiplier.v
+++ 32-bit multiplier/Multiplier.v
@@ -1,27 +1,27 @@
-module multiplier (in1,in2,final);
+module divider (in1,in2,final);
 
 // initalize i/o port
 input [31:0] in1,in2;
 output [31:0]final;
 
 // interim wire
-wire [22:0]M1,M2;
+wire [23:0]M1,M2;
 wire [7:0]E1,E2;
 wire s1,s2;
-wire[8:0] eigth_bit_adder,nine_bit_sub;
+wire[8:0] eigth_bit_sub,nine_bit_add;
 wire [47:0] multi;
 
 // Design of the circuit
 
-assign M1 =  in1[22:0];
-assign M2 =  in2[22:0];
+assign M1 = {1'b1, in1[22:0]};
+assign M2 = {1'b1, in2[22:0]};
 assign E1 =  in1[30:23];
 assign E2 =  in2[30:23];    
 
-assign eigth_bit_adder = E1 + E2;
-assign nine_bit_sub = eigth_bit_adder - 9'b001111111;
+assign eigth_bit_sub = E1 + E2;
+assign eigth_bit_add = eigth_bit_sub - 9'b001111111;
 
-assign  final[30:23] = nine_bit_sub;
+assign  final[30:23] = eigth_bit_add;
 
 assign multi = M1*M2;
 
